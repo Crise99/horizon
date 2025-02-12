9118688ac61806eee5dd32c81d0e4f80ed4f3f21
Crise99 <cristian.s.arana@gmail.com>
2025-02-12 11:35:31 +0100

    nuevo from

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -6,7 +6,7 @@ const resend = new Resend(import.meta.env.RESEND_API_KEY);
 export const GET: APIRoute = async () => {
     // send an email
     const {data, error} = await resend.emails.send({
-        "from": "Boda Nuria y Cristian <cristian.s.arana@gmail.com>",
+        "from": "Boda Nuria y Cristian <confirmaciones@bodanuriaycristian.com>",
         "to": ["cristian.s.arana@gmail.com"],
         "subject": "Nueva Confirmación",
         "html": "<strong>it works!</strong>",
