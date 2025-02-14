b010e481d8396abc01a9d00aa93ba39686d169b0
Crise99 <cristian.s.arana@gmail.com>
2025-02-14 10:26:17 +0100

    regreso

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -3,36 +3,19 @@ import { Resend } from "resend";
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
-export const post: APIRoute = async ({ request }) => {
-    const formData = await request.formData();
-
-    const name = formData.get("name");
-    const email = formData.get("email");
-    const telefono = formData.get("telefono");
-    const asistentes = formData.get("asistentes");
-    const message = formData.get("message");
-
+export const GET: APIRoute = async () => {
+    // send an email
     const {data, error} = await resend.emails.send({
         "from": "Boda Nuria y Cristian <onboarding@resend.dev>",
         "to": ["cristian.s.arana@gmail.com"],
         "subject": "Nueva Confirmación",
-        "html": `
-            <strong>Nueva confirmación!</strong>
-            <p>Nombre: ${name}</p>
-            <p>Email: ${email}</p>
-            <p>Teléfono: ${telefono}</p>
-            <p>Número de asistentes: ${asistentes}</p>
-            <p>Mensaje: ${message}</p>
-        `,
+        "html": "<strong>it works!</strong>",
     });
 
     if (error) {
-        return new Response(JSON.stringify({error}), {
-            headers: { "Content-Type": "application/json" }
-        });
+        return new Response(JSON.stringify({error}));
     }
 
-    return new Response(JSON.stringify({data}), {
-        headers: { "Content-Type": "application/json" }
-    }); 
+    return new Response(JSON.stringify({data})); 
+
 };
